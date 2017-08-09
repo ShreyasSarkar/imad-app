d1adf2ace95b8301188cd21f5bdf329e20a630df
SHREYAS SARKAR <30755216+ShreyasSarkar@users.noreply.github.com>
2017-08-09 18:48:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Page1',function(req,res){res.send('Page 1 is my first web server')});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
