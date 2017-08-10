d03bd499f5205da5794659bc04cceb5c20288631
SHREYAS SARKAR <30755216+ShreyasSarkar@users.noreply.github.com>
2017-08-10 16:06:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML=`New Value`;
 
 //Move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
  img.onclick=function(){
-     img.style.marginLeftt=`100px`;
+     var interval= setInterval(moveRight,100);
  };
