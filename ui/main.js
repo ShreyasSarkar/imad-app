efbbcf7f01dc0427b2daa7b242864dfc0bb5d3ba
SHREYAS SARKAR <30755216+ShreyasSarkar@users.noreply.github.com>
2017-08-11 22:58:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ button.onclick=function(){
   };
   
   //Make the Request
-  response.open('GET', 'http://http://shreyassarkar98.imad.hasura-app.io/', true);
+  response.open('GET', 'http://http://shreyassarkar98.imad.hasura-app.io/counter', true);
   request.send(null);
   
 };
