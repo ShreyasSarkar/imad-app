374b6ee05c6faf1ab830c9210305ac4e2135abb6
SHREYAS SARKAR <30755216+ShreyasSarkar@users.noreply.github.com>
2017-08-13 23:34:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,49 +24,36 @@ button.onclick=function(){
 };
 
 
-  //Make a request to the counter endpoint
-  var request = new XMLHttpRequest();
-  //capture the response and store it in a variable
-  request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          //Take Some Action
-          if(request.status ===200){
-               //Capture a list of names and render it as a list
-               var names=request.responseText;
-               names=JSON.parse(names);
-    var list='';
-    
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+ names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
-          }
-      }
-      //Not done yet
-  };
-  //Make the Request
-  request.open('GET', 'http://shreyassarkar98.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);
-  
-};
-
 
 //Submit name
 var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make a request to the server and send the name
+    //Create a request object
+    var request = new XMLHttpRequest();
     
-    //Capture a list of names and render it as a list
-    
-    var names=['name1','name2','name3','name4'];
-    var list='';
+    // Capture the respnse and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            //Take Some Action
+            if(request.status === 200){
+                //Capture a list of names and render it as a list
+                var name=request.responseText;
+                names=JSON.parse();
+                var list='';
     
     for(var i=0;i<names.length;i++){
         list+='<li>'+ names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML=list;
+        }
+    }
+    };
 };
+
+//Make the Request
+  request.open('GET', 'http://shreyassarkar98.imad.hasura-app.io/submit-name?name=' +name , true);
+  request.send(null);
+
