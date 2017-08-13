3790a708a92d3bc05992fe909ecc36af5cdafb14
SHREYAS SARKAR <30755216+ShreyasSarkar@users.noreply.github.com>
2017-08-13 23:56:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick=function(){
 
 
 //Submit name
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     //Create a request object
@@ -53,6 +52,8 @@ submit.onclick = function(){
     };
 
 //Make the Request
-  request.open('GET', 'http://shreyassarkar98.imad.hasura-app.io/submit-name?name=' +name , true);
-  request.send(null);
+    var nameInput=document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://shreyassarkar98.imad.hasura-app.io/submit-name?name=' +name , true);
+    request.send(null);
 };
